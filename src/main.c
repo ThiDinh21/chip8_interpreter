0aafdb4f36a76e7ad4ab43fb3008a86f1a8a7734
Thi Dinh <vietthidinh2001@gmail.com>
2023-08-08 03:02:36 +0300

    add: create an event loop for SDL window

--- src/main.c
+++ src/main.c
@@ -8,22 +8,49 @@ int main()
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
     {
         fprintf(stderr, "Failed to initialize SDL2: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("CHIP-8 Interpreter", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 320, SDL_WINDOW_SHOWN);
+    if (!window)
+    {
+        fprintf(stderr, "Failed to create window: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer)
+    {
+        fprintf(stderr, "Failed to create renderer: %s\n", SDL_GetError());
+        return 1;
     }
 
     CHIP8 *cpu = CHIP8_new();
-    // cpu->pc = 0x55;
 
-    // printf("%x, %x\n", fetch_opcode(cpu), cpu->pc);
-    // printf("%x, %x\n", fetch_opcode(cpu), cpu->pc);
-    // printf("%x, %x\n", fetch_opcode(cpu), cpu->pc);
-    // printf("%x, %x\n", fetch_opcode(cpu), cpu->pc);
-    u16 opcode = 0x9230;
-    u8 firstNibble = (opcode & 0xF000) >> 12;
-    printf("%x", firstNibble);
+    SDL_Event event;
+    int quit = 0;
+
+    while (!quit)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+                quit = 1;
+            break;
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    // Clean up SDL
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     CHIP8_destroy(cpu);
-
-    // printf("Makefile is shit!");
 
     return 0;
 }
