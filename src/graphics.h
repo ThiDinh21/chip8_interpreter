8c6642e27ec5589f0c6a8e7ac3be3aa8d099fc7e
Thi Dinh <vietthidinh2001@gmail.com>
2023-08-26 22:16:17 +0300

    doc: for graphics.h

--- src/graphics.h
+++ src/graphics.h
@@ -30,20 +30,38 @@ static const SDL_Scancode scancodeMap[16] = {
 typedef uint8_t u8;
 typedef uint16_t u16;
 
+/// @brief Initiate SDL Window, Renderer and Texture
 void initSDL(void);
 
+/// @brief Clean up SDL components
 void destroySDL(void);
 
+/// @brief Clear the screen by setting the CHIP8's display to 0
+/// @param cpu a pointer of type CHIP8 *
 void cls(CHIP8 *cpu);
 
+/// @brief Update the CHIP8's dispay with the new sprite
+/// @param cpu a pointer of type CHIP8 *, the sprite starts at the I register
+/// @param n length of the sprite
+/// @param x X coord of the display to draw
+/// @param y Y coord of the display to draw
 void drawSprite(CHIP8 *cpu, u8 n, u8 x, u8 y);
 
+/// @brief Render the CPU's display to SDL Window
+/// @param cpu a pointer of type CHIP8 *
 void renderToScreen(CHIP8 *cpu);
 
+/// @brief Check a key to see if it is pressed
+/// @param key the key, represented by a number from 0x0 to 0xF
+/// @return 0 or 1 (not pressed or pressed)
 int getKeyboardState(u8 key);
 
+/// @brief Handle the user input, update the key pad accordingly
+/// @return 1 if the user pressed Esc, 0 otherwise
 int handleUserInterrupt(void);
 
+/// @brief Wait till user press something, loop otherwise
+/// @return the number represent the pressed key
 u8 waitForUserInput(void);
 
 #endif
